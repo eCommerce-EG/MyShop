0cf006d78e19d2f972e7eba4ec1fa0d6711ced0f
Pink Butterfly <113222414+pinkbutterfly14@users.noreply.github.com>
2024-12-25 18:10:14 +0200

    Delete Google-Analytics.js

--- Google-Analytics.js
+++
@@ -1,5 +0,0 @@
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag(&#39;js&#39;, new Date());
-
-      gtag(&#39;config&#39;, &#39;G-T5H4J1RNXN&#39;);
