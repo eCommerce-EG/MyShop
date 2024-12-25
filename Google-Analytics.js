89341e0dfa40afb329992ee62a6dba1ba609780a
Pink Butterfly <113222414+pinkbutterfly14@users.noreply.github.com>
2024-12-25 18:08:02 +0200

    Update Google-Analytics.js

--- Google-Analytics.js
+++ Google-Analytics.js
@@ -1,9 +1,5 @@
-    <!-- Google tag (gtag.js) -->
-    <script async='async' src='https://www.googletagmanager.com/gtag/js?id=G-T5H4J1RNXN'/>
-    <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag(&#39;js&#39;, new Date());
 
       gtag(&#39;config&#39;, &#39;G-T5H4J1RNXN&#39;);
-    </script>
